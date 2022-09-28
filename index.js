5b821373936a52d77d65b457c6577770760524c1
cakinn <caakin01@wsc.edu>
2022-09-28 13:16:56 -0500

    test sync

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ return (
 <div>
 <div className="board-row">
 {this.renderSquare(1)}
+&nbsp;
 {this.renderSquare(2)}
 {this.renderSquare(3)}
 </div>
@@ -196,7 +197,7 @@ return squares[a];
 }
 return null;
 
-function MyForm() {
+function DescriptionTB() {
   const [textarea, setTextarea] = useState(
     "The content of a textarea goes in the value attribute"
   );
