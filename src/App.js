dcf660d4693a3036cf676c7c54d1e12d4b663701
comcco02 <97971850+comcco02@users.noreply.github.com>
2022-10-03 03:46:28 -0500

    created working site, TODO: hide/show

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function App() {
+
+   return (
+  <>
+    <h1>Welcome to Totally Not the Gag!</h1>
+    <h2>Your Feedback goes directly to our chefs.</h2>
+    What did you order?<br></br>
+    <button class="top">🍕</button>
+    <button class="top">🍟</button>
+    <button class="top">🥧</button>
+    <br></br>
+    &nbsp; Is your issue with....
+    <br></br>
+    <button class="mid">Doneness?</button><br></br>
+    <button class="mid">Ingredients?</button><br></br>
+    <button class="mid">Temperature?</button><br></br>
+    <button class="mid">Appearance?</button><br></br>
+    <br></br>
+    Please tell us what was wrong or what we could do to improve.<br></br>
+    &nbsp;<input type="text" /><br></br>
+    <button>Submit Feedback</button>
+    
+    </>
+  );
+}
+
+export default App;
