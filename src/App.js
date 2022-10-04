5cdab70eaef8713910c0a48e2f695fcbfa7b3f08
cakinn <caakin01@wsc.edu>
2022-10-04 00:33:48 -0500

    can now toggle viewing questions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-function App() {
+import { useState } from 'react';
 
-   return (
-  <>
-    <h1>Welcome to Totally Not the Gag!</h1>
-    <h2>Your Feedback goes directly to our chefs.</h2>
-    What did you order?<br></br>
-    <button class="top">🍕</button>
-    <button class="top">🍟</button>
-    <button class="top">🥧</button>
-    <br></br>
-    &nbsp; Is your issue with....
-    <br></br>
-    <button class="mid">Quality?</button><br></br>
-    <button class="mid">Ingredients?</button><br></br>
-    <button class="mid">Temperature?</button><br></br>
-    <button class="mid">Appearance?</button><br></br>
-    <br></br>
-    Please tell us what was wrong or what we could do to improve.<br></br>
-    &nbsp;<input type="text" /><br></br>
-    <button>Submit Feedback</button>    
-    
+function App() {
+  // Sets seeing questions to false at start
+  const [showQs, setShowQs] = useState(false);
+  // When food item is clicked, toggles seeing user questions
+  const onClick = () => {
+    setShowQs(current => !current);
+    //setShowQs(true);
+  }
+  const Questions = () => (
+    <div>
+      &nbsp; Is your issue with....
+      
+      <button class="mid">Quality?</button><br></br>
+      <button class="mid">Ingredients?</button><br></br>
+      <button class="mid">Temperature?</button><br></br>
+      <button class="mid">Appearance?</button><br></br>
+    </div>
+  );
+
+  return (
+    <>
+      <h1>Welcome to Totally Not the Gag!</h1>
+      <h2>Your Feedback goes directly to our chefs.</h2>
+      What did you order?<br></br>
+      <button class="top" onClick={onClick}>🍕 Pizza</button>
+      <button class="top" onClick={onClick}>🍟 Fries</button>
+      <button class="top" onClick={onClick}>🌭 Hot Dog</button>
+      
+      {/* Questions about food won't show until user clicks a button */}
+      {showQs ? <Questions /> : null }
+
+      {/* submit button displays console message saying feedback is "submitted"
+      --we aren't storing it anywhere :P*/}
+      Please tell us what was wrong or what we could do to improve.<br></br>
+      &nbsp;<input type="text" /><br></br>
+      <button onClick={() => console.log("Feedback Submitted.")}>Submit Feedback</button>
+
     </>
   );
 }
