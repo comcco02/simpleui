15a4672c3df9283a5d0674b138233f231b9d568d
comcco02 <97971850+comcco02@users.noreply.github.com>
2022-10-05 10:00:30 -0500

    updated name of app to fit readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+/*
+  Context: We are creating a system that allows a customer to judge/review their food and leave a ticket to the cooks about 
+  what went wrong, what could be improved on, and what they thought about the meal. This system ideally would run on a tablet or 
+  similar device running alone at a table and reporting results back to the owners, but this is a web app, 
+  which means we can expect it to run on almost anything. 
+*/
+
 function App() {
   // Sets seeing questions to false at start
   const [showQs, setShowQs] = useState(false);
@@ -11,7 +18,7 @@ function App() {
   const Questions = () => (
     <div>
       <p>Is your issue with....</p>
-      
+
       <button class="mid">Taste?</button><br></br>
       <button class="mid">Incorrect Order?</button><br></br>
       <button class="mid">Temperature?</button><br></br>
@@ -21,15 +28,15 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to Totally Not the Gag!</h1>
+      <h1>ConveyUr Cuisine</h1>
       <h2>Your Feedback goes directly to our chefs.</h2>
       <p>What did you order?</p><br></br>
       <button class="top" onClick={onClick}>🍕 Pizza</button>
       <button class="top" onClick={onClick}>🍟 Fries</button>
       <button class="top" onClick={onClick}>🌭 Hot Dog</button>
-      
+
       {/* Questions about food won't show until user clicks a button */}
-      {showQs ? <Questions /> : null }
+      {showQs ? <Questions /> : null}
 
       {/* submit button displays console message saying feedback is "submitted"
       --we aren't storing it anywhere :P*/}
