b59c2469482e404b5b90ef3625438f0804035f6b
comcco02 <97971850+comcco02@users.noreply.github.com>
2022-10-05 10:23:57 -0500

    default hide input text field to encourage choice

--- src/App.js
+++ src/App.js
@@ -14,19 +14,35 @@ import { useState } from 'react';
 function App() {
   // Sets seeing questions to false at start
   const [showQs, setShowQs] = useState(false);
+  const [showInput, setShowInput] = useState(false);
   // When food item is clicked, toggles seeing user questions
   const onClick = () => {
     setShowQs(current => !current);
     //setShowQs(true);
   }
+  const onClick_question = () => {
+    setShowInput(current => !current);
+    //setShowInput(true);
+  }
+  //Our div containing our list of issues
   const Questions = () => (
     <div>
       <p>Is your issue with....</p>
 
-      <button class="mid">Taste?</button><br></br>
-      <button class="mid">Incorrect Order?</button><br></br>
-      <button class="mid">Temperature?</button><br></br>
-      <button class="mid">Appearance?</button><br></br>
+      <button class="mid" onClick={onClick_question}>Taste?</button><br></br>
+      <button class="mid" onClick={onClick_question}>Incorrect Order?</button><br></br>
+      <button class="mid" onClick={onClick_question}>Temperature?</button><br></br>
+      <button class="mid" onClick={onClick_question}>Appearance?</button><br></br>
+    </div>
+  );
+
+  const Input = () => (
+    <div>
+      {/* submit button displays console message saying feedback is "submitted"
+      --we aren't storing it anywhere :P*/}
+      <p>Please tell us what was wrong or what we could do to improve.</p><br></br>
+      <input class="inputbox" type="text" /><br></br>
+      <button onClick={() => console.log("Feedback Submitted.")}>Submit Feedback</button>
     </div>
   );
 
@@ -41,12 +57,8 @@ function App() {
 
       {/* Questions about food won't show until user clicks a button */}
       {showQs ? <Questions /> : null}
-
-      {/* submit button displays console message saying feedback is "submitted"
-      --we aren't storing it anywhere :P*/}
-      <p>Please tell us what was wrong or what we could do to improve.</p><br></br>
-      <input type="text" /><br></br>
-      <button onClick={() => console.log("Feedback Submitted.")}>Submit Feedback</button>
+      {/*don't show input box until user clicks a question*/}
+      {showInput ? <Input /> : null}
 
     </>
   );
